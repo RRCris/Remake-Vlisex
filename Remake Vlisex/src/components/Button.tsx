c7846a11afd58586b56b2115b734780fe0af9d5f
RRCris <curiosity.cdrr@gmail.com>
2023-10-25 05:43:20 -0500

    starting proyect and add button y typografy

---
+++ Remake Vlisex/src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Typografy from "./Typografy";
+
+export default function Button() {
+  return (
+    <div>
+      <Typografy />
+    </div>
+  );
+}
---
+++ Remake Vlisex/src/components/Typografy.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Typografy() {
+  return <div>Typografy</div>;
+}
