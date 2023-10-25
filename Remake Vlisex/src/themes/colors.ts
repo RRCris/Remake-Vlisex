74926acb7e6742b1cf54b3788a7c539bb107821e
RRCris <curiosity.cdrr@gmail.com>
2023-10-25 07:52:39 -0500

    add the colors on figma and new info in README

--- Remake Vlisex/src/themes/colors.ts
+++ Remake Vlisex/src/themes/colors.ts
@@ -1,1 +1,16 @@
-export default {};
+export default {
+  primary: {
+    main: "#232833",
+    upper: "#353B47",
+    contrast: "#F0F0F0",
+    disabled: "#615F5F",
+    enfasis: "#FFFFFF",
+  },
+  secondary: {
+    main: "#EEEEEE",
+    upper: "#D9D9D9",
+    contrast: "#515151",
+    disabled: "#A0A0A0",
+    enfasis: "#000000",
+  },
+};
