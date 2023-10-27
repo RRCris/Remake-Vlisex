1fe4c73b116786a4b85f6657faf50f07c9b0bcd4
RRCris <curiosity.cdrr@gmail.com>
2023-10-27 07:20:58 -0500

    try add path alias && add colors ro provider theme

--- Remake Vlisex/src/providers/ProviderTheme.tsx
+++ Remake Vlisex/src/providers/ProviderTheme.tsx
@@ -1,8 +1,9 @@
 import React, { ReactNode } from "react";
+import colors from "../themes/colors";
 
 const contextTheme = React.createContext({});
 export default function ProviderTheme({ children }: { children: ReactNode }) {
-  const [theme, setTheme] = React.useState({});
+  const [theme, setTheme] = React.useState(colors);
   return (
     <contextTheme.Provider value={theme}>{children}</contextTheme.Provider>
   );
