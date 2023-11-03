474970dbbc5c68a9a3aa734f3a19834d59e58635
RRCris <curiosity.cdrr@gmail.com>
2023-11-03 17:45:46 -0500

    add component image to lazy load place holder and load animation && add config for import Alias @

--- Remake Vlisex/src/App.tsx
+++ Remake Vlisex/src/App.tsx
@@ -4,6 +4,8 @@ import { useFocusWindow } from "./hooks/useFocusWindow";
 import { useOnline } from "./hooks/useOnline";
 import { useResponsive } from "./hooks/useResponsive";
 import { useSchemePrefers } from "./hooks/useColorScheme";
+import { generateArray } from "./utilities/generateArray";
+import Image from "./components/Image";
 
 function App() {
   const focus = useFocusWindow();
@@ -16,7 +18,6 @@ function App() {
       style={{
         fontFamily: "'Manrope',sans-serif",
         background: theme === "dark" ? "#555" : "#ddd",
-        fontWeight: 400,
       }}
     >
       {focus && <h1>Hola mundo</h1>}
@@ -29,6 +30,14 @@ function App() {
       <Typografy />
 
       <h3>tu pantalla es de tamaño {width}</h3>
+      {generateArray(15).map((i) => (
+        <Image
+          src={`https://picsum.photos/id/${i}/500`}
+          alt={`Image Alt-${i}`}
+          width={700}
+          height={500}
+        />
+      ))}
     </div>
   );
 }
